e3bcd6f4ac283e30480e36be23e599583d80db51
Ben Lyon <benbenlyon@gmail.com>
2022-10-17 20:24:30 -0700

    working task 1

--- task1hashWIthDict.py
+++ task1hashWIthDict.py
@@ -22,19 +22,24 @@ def hash_and_truncate(m, size):
 def collision_stats(m0, size):
     # Given a message and the size of hash in bits, repeatedly generate and hash numbers til a collision is reached
     # Then output stats on how long it took and how many total inputs were generated
+    hashDict = {}
     start = time.time()
-    h0 = hash_and_truncate(m0, size)
-    m1 = ''.join(random.choices(string.ascii_lowercase, k=8)) # Generate 8-char string
-    h1 = hash_and_truncate(m1, size)
+    # h0 = hash_and_truncate(m0, size)
+    # m1 = ''.join(random.choices(string.ascii_lowercase, k=8)) # Generate 8-char string
+    # h1 = hash_and_truncate(m1, size)
     inputs = 1
-    while((h1 != h0) and (m1 != m0)):
+    while(1):
         m1 = ''.join(random.choices(string.ascii_lowercase, k=8)) # Generate new string
         h1 = hash_and_truncate(m1, size)
+        # print(h1)
+        if(hashDict.get(h1) != None and hashDict.get(h1) != m1):
+            break
+        hashDict.update({h1:m1})
         inputs += 1
     end = time.time()
     print("Collision found!")
     print("Input message: " + m0)
-    print("Input hash bin: " + h0)
+    # print("Input hash bin: " + h0)
     print("Colliding message: " + m1)
     print("Colliding hash bin: " + h1)
     print("Input Size: " + str(size))
@@ -62,7 +67,7 @@ def main():
         writer = csv.writer(file)
         writer.writerow(header)
     # Gather collision stats for every even-length size from 8 to 50
-        for size in range(8, 50, 2):
+        for size in range(8, 52, 2):
             m = ''.join(random.choices(string.ascii_lowercase, k=8))
             writer.writerow(collision_stats(m, size))
 
